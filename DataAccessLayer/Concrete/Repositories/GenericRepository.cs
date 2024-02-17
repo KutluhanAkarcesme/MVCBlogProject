6bfd93e6ceb49c5d9f48ba317b3321dea5feafa7
KutluhanAkarcesme <kutluhanakarcesme@gmail.com>
2024-02-17 14:33:49 +0300

    Created WriterPage

--- DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -20,13 +20,15 @@ namespace DataAccessLayer.Concrete.Repositories
         }
         public void Add(T entity)
         {
-            _object.Add(entity);
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
             context.SaveChanges();
         }
 
         public void Delete(T entity)
         {
-            _object.Remove(entity);
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
             context.SaveChanges();
         }
 
@@ -42,6 +44,8 @@ namespace DataAccessLayer.Concrete.Repositories
 
         public void Update(T entity)
         {
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
             context.SaveChanges();
         }
 
---
+++ UI.Web/Controllers/WriterController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Web.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EFWriterDal());
+
+        public ActionResult Index()
+        {
+            var writerValues = writerManager.GetList();
+            return View(writerValues);
+        }
+    }
+}
