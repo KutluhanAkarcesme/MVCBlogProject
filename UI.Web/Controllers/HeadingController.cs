0caa5871ba7387b0529ee2428df1da8225361d8d
KutluhanAkarcesme <kutluhanakarcesme@gmail.com>
2024-03-01 17:20:49 +0300

    Development with HeadingController

--- UI.Web/Controllers/HeadingController.cs
+++ UI.Web/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,33 @@ namespace UI.Web.Controllers
     public class HeadingController : Controller
     {
         HeadingManager headingManager = new HeadingManager(new EFHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
         public ActionResult Index()
         {
             var headingValues = headingManager.GetList();
             return View(headingValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> categoryValues = (from x in categoryManager.GetList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryId.ToString()
+                                                   }).ToList();
+            ViewBag.vlc = categoryValues;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            headingManager.Add(heading);
+
+            return RedirectToAction("Index");
         }
     }
 }
