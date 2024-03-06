c3afcfa1806a0b381d416741056bb7fb5cfd9cb7
KutluhanAkarcesme <kutluhanakarcesme@gmail.com>
2024-03-06 16:47:35 +0300

    Created contentService

---
+++ BusinessLayer/Abstract/IContentService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IContentService
+    {
+        List<Content> GetList();
+        Content GetById(int id);
+        List<Content> GetListById(int id);
+        void Add(Content content);
+        void Delete(Content content);
+        void Update(Content content);
+    }
+}
---
+++ BusinessLayer/Concrete/ContentManager.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContentManager : IContentService
+    {
+        IContentDal _contentDal;
+        public ContentManager(IContentDal contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public void Add(Content content)
+        {
+            _contentDal.Add(content);
+        }
+
+        public void Delete(Content content)
+        {
+            _contentDal.Delete(content);
+        }
+
+        public Content GetById(int id)
+        {
+            return _contentDal.GetById(x => x.ContentId == id);
+        }
+
+        public List<Content> GetList()
+        {
+            return _contentDal.GetList();
+        }
+
+        public List<Content> GetListById(int id)
+        {
+            return _contentDal.GetListByFilter(x => x.HeadingId == id);
+        }
+
+        public void Update(Content content)
+        {
+             _contentDal.Update(content);
+        }
+    }
+}
